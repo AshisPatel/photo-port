0c46a5ed377a4c9b32b69eb81df757ad1f8d545c
AP <ashis.n.patel@gmail.com>
2021-10-23 15:35:59 -0500

    Pass currentcategory props to gallery, create PhotoList component

---
+++ src/components/PhotoList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import photo from "../assets/small/commercial/0.jpg";
+
+function PhotoList() {
+
+    return (
+        <div>
+            <img
+                src={photo}
+                alt="Photo example"
+            />
+        </div>
+    );
+
+}
+
+export default PhotoList;
